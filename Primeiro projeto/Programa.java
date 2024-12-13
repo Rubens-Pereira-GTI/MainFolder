daf9513c1bf36830bcdd4faf789ba2082b728fc4
Rubens-Pereira-GTI <rubens.costa.tkd@gmail.com>
2024-12-12 21:01:24 -0300

    Update Programa.java

--- Primeiro projeto/Programa.java
+++ Primeiro projeto/Programa.java
@@ -1,6 +1,17 @@
 public class Programa {
-    
+        public static int num =1;
+        public String nome="rubens";
+
+        public void go(){
+            nome = "Carlos";
+        
+    }
+        
     public static void main(String[] args) {
         System.out.println("olá");
+        Programa p = new Programa();
+        p.go();
+        System.out.println( Programa.num);
+        System.out.println( p.nome);
     }
 }
