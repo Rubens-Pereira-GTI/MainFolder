b3fcd3ccaaa577358a5faf22fa66a318c01aeaab
Rubens-Pereira-GTI <rubens.costa.tkd@gmail.com>
2025-03-21 23:52:14 -0300

    21032025

---
+++ Learning/arvoreBinaria/BinarySearch.java
@@ -0,0 +1,55 @@
+package arvoreBinaria;
+
+public class BinarySearch {
+    // Returns index of x if it is present in arr[].
+    int binarySearch(int arr[], int x)
+    {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // Check if x is present at mid
+            if (arr[mid] == x)
+                return mid;
+
+            // If x greater, ignore left half
+            if (arr[mid] < x)
+                low = mid + 1;
+
+            // If x is smaller, ignore right half
+            else
+                high = mid - 1;
+        }
+
+        // If we reach here, then element was
+        // not present
+        return -1;
+    }
+
+    // Driver code
+    public static void main(String args[])
+    {
+        BinarySearch ob = new BinarySearch();
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int n = arr.length;
+        int x = 10;
+        //int []num = ob.teste();
+        //System.out.println(num[10]);
+        int result = ob.binarySearch(ob.teste(), x);
+        if (result == -1)
+            System.out.println(
+                "Element is not present in array");
+        else
+            System.out.println("Element is present at "
+                               + "index " + result);
+    }
+
+    public int[] teste(){
+        int[]y = new int[100];
+        for( int x=0; x<100; x++){
+            y[x]=x;            
+        }
+        //System.out.println(y[11]);
+        return y;
+    }
+}
